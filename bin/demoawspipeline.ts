2e8f9030631a88b3df11da0c4c121ed0da677007
Mohd Husain Ahmed <mohdhusainahmed001@gmail.com>
2024-12-15 21:12:23 +0530

    first-commit

---
+++ bin/demoawspipeline.ts
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+import * as cdk from 'aws-cdk-lib';
+import { DemoawspipelineStack } from '../lib/demoawspipeline-stack';
+
+const app = new cdk.App();
+new DemoawspipelineStack(app, 'DemoawspipelineStack', {
+  env: { account: '123456789012', region: 'us-east-1' },
+});
---
+++ lib/demoawspipeline-stack.ts
@@ -0,0 +1,21 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
+
+export class DemoawspipelineStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // AWS CI-CD Pipeline
+
+    const democicdpipeline = new CodePipeline(this, 'demopipeline', {
+      synth: new ShellStep('Synth', {
+        // Use a connection created using the AWS console to authenticate to GitHub
+        // Other sources are available.
+        input: CodePipelineSource.gitHub(
+          'mohdhusainahmed001 / demoawspipeline', 'main'),
+        commands: ['npm ci', 'npm run build', 'npx cdk synth'],
+      }),
+    });
+  }
+}
